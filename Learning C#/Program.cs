206bc81cd2e1e622d613d23368bc7c464fc76040
IshwarJaee <105958105+IshwarJaee@users.noreply.github.com>
2026-01-27 20:27:11 +0530

    Initial Commmit

---
+++ Learning C#/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public class Calc
+    {
+        public static bool AreEqual(int a, int b)
+        {
+            return a.Equals(b);
+        }
+    }
+    public static void Main(string[] args)
+    { 
+        bool c = Calc.AreEqual(2, 2);
+        if(c== true)
+        Console.WriteLine("yes");
+    }
+}
---
+++ SimpleMVCCRUDApp/Data/StudentmvcDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleMVCCRUDApp.Models;
+
+namespace SimpleMVCCRUDApp.Data
+{
+    public class StudentmvcDbContext : DbContext
+    {
+        public StudentmvcDbContext(DbContextOptions<StudentmvcDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
